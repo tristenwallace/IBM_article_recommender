c74db5bb0ad2b74068552b15f11d406921d0bce8
tristenwallace <tri3sten@gmail.com>
2024-02-18 16:39:48 -0500

    chore: Move placeholder img out of config

--- src/img_search.py
+++ src/img_search.py
@@ -9,6 +9,21 @@ nltk.download('stopwords')
 from config import unsplash_access_key
 from config import placeholder_images
 
+placeholder_images = [
+    'https://images.unsplash.com/photo-1525338078858-d762b5e32f2c?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1696258686454-60082b2c33e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YWl8ZW58MHx8MHx8fDA%3D',
+    'https://images.unsplash.com/photo-1677756119517-756a188d2d94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YWl8ZW58MHx8MHx8fDA%3D',
+    'https://images.unsplash.com/photo-1495055154266-57bbdeada43e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGFpfGVufDB8fDB8fHww',
+    'https://images.unsplash.com/photo-1674027444485-cec3da58eef4?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1666597107756-ef489e9f1f09?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1684369175809-f9642140a1bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGFpfGVufDB8fDB8fHww',
+    'https://images.unsplash.com/photo-1670163426610-69cdc930f4e0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTA2fHxhaXxlbnwwfHwwfHx8MA%3D%3D',
+    'https://images.unsplash.com/photo-1692607334827-4da64dcf2221?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTA3fHxhaXxlbnwwfHwwfHx8MA%3D%3D',
+    'https://images.unsplash.com/photo-1599941662219-f95e5d125ce7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTE0fHxhaXxlbnwwfHwwfHx8MA%3D%3D',
+    'https://images.unsplash.com/photo-1677442135131-4d7c123aef1c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTM0fHxhaXxlbnwwfHwwfHx8MA%3D%3D',
+    'https://images.unsplash.com/photo-1676411237170-ddca6e4c158a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMyfHxhaXxlbnwwfHwwfHx8MA%3D%3D'
+]
+
 def extract_keywords(title):
     stop_words = set(stopwords.words('english'))
     words = word_tokenize(title)
